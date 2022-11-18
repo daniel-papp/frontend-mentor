f794617fadbe481fef03ca94f8c1edcfe0e11cc9
Daniel Papp <papp.daniel.gabor@gmail.com>
2022-11-18 14:09:30 +0100

    Add card change on submit

--- interactive-rating-component/app.js
+++ interactive-rating-component/app.js
@@ -1,11 +1,14 @@
 // DOM caching 
 
 const card = document.querySelector('.card');
-const header = document.querySelector('header');
+const header = document.querySelector('.header');
 const text = document.querySelector('.text');
+const title = document.querySelector('h1');
+const paragraph = document.querySelector('p');
 const ratingArea = document.querySelector('.rating-area');
 
 const ratings = document.querySelectorAll('.rating');
+const submitButton = document.querySelector('#submit-btn');
 
 // Declaring variables
 
@@ -15,14 +18,33 @@ let chosenRating;
 
 const chooseRating = function(e) {
     ratings.forEach(rating => rating.classList.remove('active'));
-    
+
     e.target.classList.add('active');
 
     chosenRating = e.target.innerText;
-    console.log(chosenRating);
-
 }
 
-// Assigning eventlisteners
+const createThankYouCard = function() {
+    console.log(`Thank You!`);
+    console.log(`You selected ${chosenRating} out of 5`);
+
+
+    text.classList.add('center-align');
+    title.textContent = 'Thank You!';
+    paragraph.textContent = 'We appreciate you taking the time to give a rating. If you ever need more support, don\'t hesitate to get in touch!';
+    card.removeChild(ratingArea);
+}
+
+const submitRating = function() {
+    if (chosenRating) {
+        createThankYouCard();
+    }
+}
+
+
+
+// Assigning event listeners
 
 ratings.forEach(rating => rating.addEventListener('click', chooseRating));
+
+submitButton.addEventListener('click', submitRating);
