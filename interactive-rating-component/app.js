8d1e75ed1aee46421f27ecadcbba74e07d92ef8a
Daniel Papp <papp.daniel.gabor@gmail.com>
2022-11-21 19:02:07 +0100

    Implement module pattern

--- interactive-rating-component/app.js
+++ interactive-rating-component/app.js
@@ -1,59 +1,95 @@
-// DOM caching 
+// Display Controller
 
-const card = document.querySelector('.card');
-const header = document.querySelector('.header');
-const text = document.querySelector('.text');
-const title = document.querySelector('h1');
-const paragraph = document.querySelector('p');
-const ratingArea = document.querySelector('.rating-area');
+const display = (function() {
 
-const ratings = document.querySelectorAll('.rating');
-const submitButton = document.querySelector('#submit-btn');
+    // DOM caching
 
-// Declaring variables
+    const card = document.querySelector('.card');
+    const header = document.querySelector('.header');
+    const text = document.querySelector('.text');
+    const title = document.querySelector('h1');
+    const paragraph = document.querySelector('p');
+    const ratingArea = document.querySelector('.rating-area');
 
-let chosenRating;
+    const ratings = document.querySelectorAll('.rating');
+    const submitButton = document.querySelector('#submit-btn');
 
-// Functions
 
-const chooseRating = function(e) {
-    ratings.forEach(rating => rating.classList.remove('active'));
-
-    e.target.classList.add('active');
-
-    chosenRating = e.target.innerText;
-}
-
-const createThankYouCard = function() {
-    console.log(`Thank You!`);
-    console.log(`You selected ${chosenRating} out of 5`);
-
-    header.removeChild(document.querySelector('.star-container'));
-    header.classList.remove('left-align');
-
-    const thankYouImg = document.createElement('img');
-    thankYouImg.setAttribute('src', './assets/illustration-thank-you.svg');
-    header.appendChild(thankYouImg);
-
-    const ratingText = document.createElement('p');
-    ratingText.textContent = `You selected ${chosenRating} out of 5`;
-    ratingText.classList.add('rating-text');
-    header.appendChild(ratingText);
-
-    text.classList.add('center-align');
-    title.textContent = 'Thank You!';
-    paragraph.textContent = 'We appreciate you taking the time to give a rating. If you ever need more support, don\'t hesitate to get in touch!';
-
-    card.removeChild(ratingArea);
-}
-
-const submitRating = function() {
-    if (chosenRating) {
-        createThankYouCard();
+    const renderThxCard = function() {
+        createThxImage();
+        createRatingText();
+        changeText();
+        removeRatingArea();
     }
-}
 
-// Assigning event listeners
+    const createThxImage = function() {
+        header.removeChild(document.querySelector('.star-container'));
+        header.classList.remove('left-align');
+    
+        const thankYouImg = document.createElement('img');
+        thankYouImg.setAttribute('src', './assets/illustration-thank-you.svg');
+        header.appendChild(thankYouImg);
+    }
 
-ratings.forEach(rating => rating.addEventListener('click', chooseRating));
-submitButton.addEventListener('click', submitRating);
+    const createRatingText = function() {
+        const ratingText = document.createElement('p');
+        ratingText.textContent = `You selected ${ratingComponentController.getRating()} out of 5`;
+        ratingText.classList.add('rating-text');
+        header.appendChild(ratingText);
+    }
+
+    const changeText = function() {
+        text.classList.add('center-align');
+        title.textContent = 'Thank You!';
+        paragraph.textContent = 'We appreciate you taking the time to give a rating. If you ever need more support, don\'t hesitate to get in touch!';
+    }
+
+    const removeRatingArea = function() {
+        card.removeChild(ratingArea);
+    }
+
+
+    return {
+        ratings,
+        submitButton,
+        renderThxCard
+    }
+
+})();
+
+
+// Rating Component Controller
+
+const ratingComponentController = (function() {
+
+    let chosenRating;
+
+    const chooseRating = function(e) {
+        display.ratings.forEach(rating => rating.classList.remove('active'));
+    
+        e.target.classList.add('active');
+    
+        chosenRating = e.target.innerText;
+    }
+
+    const submitRating = function() {
+        if (chosenRating) {
+            display.renderThxCard();
+        }
+    }
+
+    const getRating = function() {
+        return chosenRating
+    }
+
+    // binding events
+
+    display.ratings.forEach(rating => rating.addEventListener('click', chooseRating));
+    display.submitButton.addEventListener('click', submitRating);
+
+
+    return {
+        getRating
+    }
+
+})();
