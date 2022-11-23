836f6f131f6dd53088bef47cff513911346f834c
Daniel Papp <papp.daniel.gabor@gmail.com>
2022-11-23 18:54:55 +0100

    Refactor code

--- intro-component-with-signup-form/app.js
+++ intro-component-with-signup-form/app.js
@@ -1,19 +1,15 @@
 const formValidator = (function() {
 
     const submitButton = document.querySelector('.submit-btn');
-    const userFirstName = document.querySelector('#user-firstname');
-    const userLastName = document.querySelector('#user-lastname');
-    const userEmail = document.querySelector('#user-email');
-    const userPass = document.querySelector('#user-pass');
-
+    const userFields = document.querySelectorAll('input');
 
     const checkEmptyFields = function() {
-        if (!userFirstName.checkValidity()) userFirstName.removeAttribute('placeholder');
-        if (!userLastName.checkValidity()) userLastName.removeAttribute('placeholder');
-        if (!userEmail.checkValidity()) userEmail.removeAttribute('placeholder');
-        if (!userPass.checkValidity()) userPass.removeAttribute('placeholder');
+
+        for (let field of userFields) {
+            if (!field.checkValidity()) field.removeAttribute('placeholder');
+        }
     }
 
     submitButton.addEventListener('click', checkEmptyFields);
-    
+
 })()
